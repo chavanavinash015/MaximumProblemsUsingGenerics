257aa96c69cb1527ff160cec074f3d6c4a7f7e54
arc-015 <chavanavinash015@gmail.com>
2022-10-31 22:19:46 +0530

    Welcome to the Generics Problems

---
+++ src/com/bridgelabz/Generics/MaximumNumber.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Generics;
+
+public class MaximumNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Generics Problems");
+
+    }
+}
